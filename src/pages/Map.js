d60c19613c09fa51a51e4b512f4d839e144e65a7
kamal <kamalourajdal@gmail.com>
2023-09-04 20:59:32 +0100

    map page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import "leaflet/dist/leaflet.css";
+import ReactDOM from "react-dom";
+import cloudy from "../media/icons/sunset_icon.png";
 
-function Map() {
+import { MapContainer, TileLayer, MapConsumer } from "react-leaflet";
+import L from "leaflet";
+
+import SearchBar from "../components/SearchBar";
+
+const Map = () => {
+  const CustomPopupContent = async () => {
+    {
+      const content = document.createElement("div");
+      ReactDOM.render(
+        <div className="w-32 flex flex-col space-y-0">
+          <span className="font-bold text-lg  mx-auto">Agadir</span>
+          <div className="flex items-center justify-between">
+            <img src={cloudy} alt="" className="w-10" />
+            <p className="text-xl font-bold">36 C</p>
+          </div>
+        </div>,
+        content
+      );
+      return content;
+    }
+  };
   return (
-    <div>
-      Map carte
-    </div>
-  )
-}
+    <div className=" w-full">
+      <SearchBar />
 
-export default Map
+      <div className="w-full h-full mr-4 pb-14 flex ">
+        <MapContainer
+          center={[31.61, -7.61]}
+          zoom={5}
+          scrollWheelZoom={true}
+          className="w-full h-full rounded-xl top-4 shadow-md mr-2"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <MapConsumer>
+            {(map) => {
+              map.on("click", async function (e) {
+                const { lat, lng } = e.latlng;
+                // show popup in clicked place on the map
+                L.popup()
+                  .setLatLng(e.latlng)
+                  .setContent(await CustomPopupContent())
+                  .openOn(map);
+              });
+
+              return null;
+            }}
+          </MapConsumer>
+        </MapContainer>
+        <div className="h-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
