26660b86f7dda8ea5a8b1d44b5cb8885fd002fab
Kamal Ourajdal <kamalourajdal@berexia.com>
2025-07-11 23:22:21 +0100

    fix(SearchBar.jsx): update API URL to use secure HTTPS endpoint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -57,7 +57,7 @@ const SearchBar = ({ onSearchChange, setLocation }) => {
     setLoading(true);
     try {
       const response = await fetch(
-          `https://api.geonames.org/searchJSON?name_startsWith=${query}&maxRows=10&username=${USERNAME}`
+          `https://secure.geonames.org/searchJSON?name_startsWith=${query}&maxRows=10&username=${USERNAME}`
       );
       const data = await response.json();
       const citySuggestions = data.geonames.map((city) => {
